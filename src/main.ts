c5be4abdf9875921128740f14bc9bf5d3b50c0bf
jongwoo <jongwoo@vinnsmail.com>
2025-07-11 12:07:11 +0900

    CORS 추가 전광판 개발

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
       'http://localhost:3030',
       'http://localhost:5173',
       'https://dsign.zigdding.com',
+      'https://dev-dsign.zigdding.com',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
